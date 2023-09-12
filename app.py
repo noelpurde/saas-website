9d696d90247b2996ca87a7d8060040a5726acee0
Noel Purde <noelpurde@gmail.com>
2023-09-12 14:59:21 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+
+# def index():
+#     return "<h1> World</h1>"
+
+def index():
+    first_name = "John"
+    stuff = "This is <strong> Bold </strong> Text"
+    return render_template("index.html", first_name=first_name, stuff=stuff)
+
+# localhost:500/user/Noel
+@app.route('/user/<name>')
+
+def user(name):
+    return render_template("user.html", user_name=name)
+
+
+#Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+#Internal Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
