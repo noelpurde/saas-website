8ffb4991f33e837e299bc0838a356e5743c5ad1c
Stiven Qehaja <120978440+stivenqehaja@users.noreply.github.com>
2024-02-01 00:18:48 +0100

    Scope "profile'

--- app.py
+++ app.py
@@ -250,7 +250,7 @@ def linkedin_signin():
         'client_id': CLIENT_ID,
         'redirect_uri': REDIRECT_URI,
         'state': STATE,
-        'scope': 'openid',
+        'scope': 'profile',
     }
     auth_url = f"{AUTHORIZATION_URL}?{'&'.join([f'{k}={v}' for k, v in params.items()])}"
 
