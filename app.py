03371cea2d1d13ba66584ede72c3f892a4303045
Stiven Qehaja <120978440+stivenqehaja@users.noreply.github.com>
2024-02-01 11:00:35 +0100

    Thonjza

--- app.py
+++ app.py
@@ -250,7 +250,7 @@ def linkedin_signin():
         'client_id': CLIENT_ID,
         'redirect_uri': REDIRECT_URI,
         'state': STATE,
-        'scope': ''openid profile w_member_social email',
+        'scope': 'openid profile w_member_social email',
     }
     auth_url = f"{AUTHORIZATION_URL}?{'&'.join([f'{k}={v}' for k, v in params.items()])}"
 
